430892b243384d9509d03f01f4e3c95a330173e0
Connor McGuire SE2019 <connor.mcguire.2020@uni.strath.ac.uk>
2023-11-19 01:38:39 +0000

    -Included chart.js for algorithm visualisation

--- FrontEnd/Univeristy-Dissertation/src/Pages/SortingVisualiser/BubbleSort/BubbleSort.tsx
+++ FrontEnd/Univeristy-Dissertation/src/Pages/SortingVisualiser/BubbleSort/BubbleSort.tsx
@@ -1,12 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import './BubbleSort.css';
+import React, { useEffect, useState, useRef } from "react";
+import Chart from "chart.js/auto";
+import "./BubbleSort.css";
 
 interface BubbleSortAnimationProps {
   states: number[][];
 }
 
-const BubbleSortAnimation: React.FC<BubbleSortAnimationProps> = ({ states }) => {
+const BubbleSortAnimation: React.FC<BubbleSortAnimationProps> = ({
+  states,
+}) => {
   const [currentStep, setCurrentStep] = useState(0);
+  const chartRef = useRef<HTMLCanvasElement | null>(null);
+  const chartInstance = useRef<Chart | null>(null);
 
   useEffect(() => {
     let animation: NodeJS.Timeout;
@@ -14,8 +19,8 @@ const BubbleSortAnimation: React.FC<BubbleSortAnimationProps> = ({ states }) => 
     const playAnimation = () => {
       if (currentStep < states.length - 1) {
         animation = setTimeout(() => {
-          setCurrentStep(prevStep => prevStep + 1);
-        }, 1000); // Interval Duration, maybe find a calculation for longer lists 
+          setCurrentStep((prevStep) => prevStep + 1);
+        }, 1000); // Interval Duration, maybe find a calculation for longer lists
       }
     };
 
@@ -26,18 +31,52 @@ const BubbleSortAnimation: React.FC<BubbleSortAnimationProps> = ({ states }) => 
     };
   }, [currentStep, states]);
 
+  useEffect(() => {
+    if (chartRef.current && states[currentStep]) {
+      if (chartInstance.current) {
+        chartInstance.current.data.labels = states[currentStep].map(String);
+        chartInstance.current.data.datasets[0].data = states[currentStep];
+        chartInstance.current.update();
+      } else {
+        const ctx = chartRef.current.getContext("2d");
+        if (ctx) {
+          chartInstance.current = new Chart(ctx, {
+            type: "bar",
+            data: {
+              labels: states[currentStep].map(String),
+              datasets: [
+                {
+                  label: "Bubble Sort Visualization",
+                  data: states[0],
+                  backgroundColor: "rgba(54, 162, 235, 0.5)",
+                  borderColor: "rgba(54, 162, 235, 1)",
+                  borderWidth: 1,
+                },
+              ],
+            },
+            options: {
+              indexAxis: "x",
+              scales: {
+                y: {
+                  display: false,
+                },
+              },
+              plugins: {
+                tooltip: {
+                  enabled: false,
+                },
+              },
+            },
+          });
+        }
+      }
+    }
+  }, [currentStep, states]);
+
   return (
     <div className="bubble-sort-container">
       <h2>Bubble Sort Visualization</h2>
-      {states.length > 0 && (
-        <div className="bubble-sort-state">
-          {states[currentStep].map((value, index) => (
-            <div key={index} className="sort-bar">
-              {value}
-            </div>
-          ))}
-        </div>
-      )}
+      <canvas ref={chartRef} className="bubble-sort-chart"></canvas>
     </div>
   );
 };
